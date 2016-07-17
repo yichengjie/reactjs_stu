9e8fcf10d0bc230d6170245af5402de5048ca397
yichengjie <626659321@qq.com>
2016-07-17 09:57:35 +0800

    hello world

--- demo10/src/main.jsx
+++ demo10/src/main.jsx
@@ -10,7 +10,6 @@ var PRODUCTS = [
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
 ];
- 
 ReactDom.render(
   <FilterableProductTable products={PRODUCTS} />,
   document.getElementById('app')
