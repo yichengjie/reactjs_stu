0b1031205119fb8d8e74e9b90818de663487e796
yichengjie <626659321@qq.com>
2016-07-28 22:14:10 +0800

    prettify code

--- demo08/src/main.jsx
+++ demo08/src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+//--api
 import AnswerMultipleChoiceQuestion from './components/AnswerMultipleChoiceQuestion.jsx' ;
-
 var arrayOfChoices = [
 	'苹果',"香蕉","橘子"
 ] ;
-
 function onCompleted (value){
 	console.info('你选择了最喜欢的水果 : ' + value) ;
 }
-
-ReactDom.render(<AnswerMultipleChoiceQuestion value ="橘子" choices ={arrayOfChoices} onCompleted = {onCompleted}/>,
+ReactDom.render(<AnswerMultipleChoiceQuestion value ="橘子"
+		choices ={arrayOfChoices} onCompleted = {onCompleted}/>,
 	document.getElementById('app')
 ) ;
