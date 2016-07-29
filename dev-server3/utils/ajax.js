fef216c5a177df4c328f927ade9ff3bf5c645131
yichengjie <626659321@qq.com>
2016-07-29 17:40:53 +0800

    react hot test

---
+++ dev-server3/utils/ajax.js
@@ -0,0 +1,19 @@
+import $ from 'jquery'; 
+
+export function get(url){
+	var defer = $.Deferred();
+	var option = {
+   	   url:serverURL,
+   	   type: 'GET',
+   	   dataType:'json',
+   	   timeout : 100000, //超时时间设置，单位毫秒
+   	   error: function (err) {
+   		   defer.reject(err) ;
+   	   },
+   	   success:function (result) { 
+   		   defer.resolve(result);
+   	   }
+	 };
+   	 $.ajax(option); //发送ajax请
+   	 return defer.promise();
+}
